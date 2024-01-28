9344c4bf8fcd637b798c470b00c79c712b1c45cb
Sz-Wei Chen <swchen1217@gmail.com>
2024-01-28 10:37:09 +0800

    Create Subsystem

---
+++ src/main/java/frc/robot/subsystems/LinkageSubsystem.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LinkageConstants;
+
+public class LinkageSubsystem extends SubsystemBase {
+  private final CANSparkMax linkageMotor = new CANSparkMax(LinkageConstants.kLinkageMotorPort,MotorType.kBrushless);
+  private final SparkAbsoluteEncoder linkageEncoder = linkageMotor.getAbsoluteEncoder(Type.kDutyCycle);
+  private SparkPIDController linkagePIDController = linkageMotor.getPIDController();
+
+  /** Creates a new LinkageSubsystem. */
+  public LinkageSubsystem() {
+    linkageMotor.setInverted(false);
+
+    // Set PID values for the Spark Max PID
+    linkagePIDController.setP(LinkageConstants.kPLinkage);
+    linkagePIDController.setI(LinkageConstants.kILinkage);
+    linkagePIDController.setD(LinkageConstants.kDLinkage);
+    linkagePIDController.setIZone(0.0);
+    linkagePIDController.setFF(0.0);
+    linkagePIDController.setOutputRange(-1, 1);
+    linkageMotor.burnFlash();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Linkage Position", linkageEncoder.getVelocity());
+  }
+
+  public void up(){
+    linkagePIDController.setReference(LinkageConstants.kUpPosition, ControlType.kPosition);
+  }
+
+  public void down(){
+    linkagePIDController.setReference(LinkageConstants.kDownPosition, ControlType.kPosition);
+  }
+
+  public void stop(){
+    linkageMotor.set(0);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final CANSparkMax topShooterMotor = new CANSparkMax(ShooterConstants.kTopShooterMotorPort,
+      MotorType.kBrushless);
+  private final CANSparkMax bottomShooterMotor = new CANSparkMax(ShooterConstants.kBottomShooterMotorPort,
+      MotorType.kBrushless);
+
+  private final RelativeEncoder shooterEncoder = topShooterMotor.getEncoder();
+  
+  private SparkPIDController shooterPIDController = topShooterMotor.getPIDController();
+
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+    topShooterMotor.setInverted(true);
+
+    bottomShooterMotor.follow(topShooterMotor);
+
+    shooterEncoder.setVelocityConversionFactor(ShooterConstants.kShooterMotorGearRatio);
+
+    // Set PID values for the Spark Max PID
+    shooterPIDController.setP(ShooterConstants.kPShooter);
+    shooterPIDController.setI(ShooterConstants.kIShooter);
+    shooterPIDController.setD(ShooterConstants.kDShooter);
+    shooterPIDController.setIZone(0.0);
+    shooterPIDController.setFF(0.0);
+    shooterPIDController.setOutputRange(0, 1);
+    topShooterMotor.burnFlash();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Shooter RPM", shooterEncoder.getVelocity());
+  }
+
+  public void setSpeed(int speed) {
+    if (speed > ShooterConstants.kShooterMotorMaxRPM)
+      speed = ShooterConstants.kShooterMotorMaxRPM;
+    shooterPIDController.setReference(speed, ControlType.kVelocity);
+  }
+
+  public void stop() {
+    topShooterMotor.set(0);
+  }
+}
