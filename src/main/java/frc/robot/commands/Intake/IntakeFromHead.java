5d45297e886bf971fe6197e2ebcb9396fea6c8e6
Sz-Wei Chen <swchen1217@gmail.com>
2024-01-29 16:00:21 +0800

    Intake from Shooter

---
+++ src/main/java/frc/robot/commands/Intake/IntakeFromHead.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Intake;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class IntakeFromHead extends Command {
+  IntakeSubsystem intakeSubsystem;
+  ShooterSubsystem shooterSubsystem;
+  Timer timer = new Timer();
+
+  /** Creates a new IntakeFromHead. */
+  public IntakeFromHead(IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
+    this.intakeSubsystem = intakeSubsystem;
+    this.shooterSubsystem = shooterSubsystem;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intakeSubsystem);
+    addRequirements(shooterSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooterSubsystem.runRev();
+    timer.reset();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    System.out.println(timer.get());
+    if(intakeSubsystem.isPass() && timer.get()==0){
+      timer.start();
+      intakeSubsystem.runRev();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    timer.stop();
+    shooterSubsystem.stop();
+    intakeSubsystem.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return timer.get() > 0.15;
+  }
+}
